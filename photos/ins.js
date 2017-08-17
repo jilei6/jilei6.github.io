0e0def93447c753571c073989e75fbacce676253
consins <consins@cubee.com>
2017-08-17 11:38:26 +0800

    Site updated: 2017-08-17 11:38:25

--- photos/ins.js
+++ photos/ins.js
@@ -117,7 +117,7 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
           var minSrc = 'https://github.com/jilei6/Blog-Back-Up/blob/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/jilei6/Blog-Back-Up/blob/master/min_photos/' + data.link[i];
+          var src = 'https://github.com/jilei6/Blog-Back-Up/blob/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
